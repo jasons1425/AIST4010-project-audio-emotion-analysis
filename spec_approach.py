5b1c6782e27bfa037bb8801c4c42c3ff5d025900
jasons1425 <jasonns1425@gmail.com>
2022-03-19 20:00:42 +0800

    Add spectrogram approach related codes

---
+++ spec_approach.py
@@ -0,0 +1,35 @@
+from data.load import load_imgs, get_loader, get_labels
+from helper.process import train_model
+from models.VGG import VGGSpecModel
+from torchvision.models import vgg16
+import torch.nn as nn
+import torch
+
+
+# data preparation
+BATCH = 32
+data, ids = load_imgs()
+train_data, valid_data = data[:8000], data[8000:]
+train_ids, valid_ids = ids[:8000], ids[8000:]
+train_labels, valid_labels = get_labels(train_ids), get_labels(valid_ids)
+train_loader = get_loader(train_data, train_labels, batch_size=BATCH)
+valid_loader = get_loader(valid_data, valid_labels, batch_size=BATCH)
+
+
+# model preparation
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model = VGGSpecModel(vgg16, 4096).half().to(device)
+
+
+# training params
+LR, MOMENTUM, DECAY = 0.0001, 0.9, 0.01
+criterion = nn.MSELoss()
+optimizer = torch.optim.SGD(model.parameters(),
+                            lr=LR, momentum=MOMENTUM, weight_decay=DECAY)
+
+
+# training
+EPOCHS = 10
+best_model, losses = train_model(model, train_loader, criterion, optimizer, EPOCHS,
+                                 device, valid_loader=valid_loader, half=True)
+torch.save(model.state_dict(), f"spec_approach.pth")
