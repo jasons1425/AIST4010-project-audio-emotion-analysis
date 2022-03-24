ab969146f01650615e6d51e8a1b23c5b0eaf8c77
jasons1425 <jasonns1425@gmail.com>
2022-03-24 16:19:55 +0800

    Update spec-approach result summary

--- spec approach/valence.py
+++ spec approach/valence.py
@@ -8,7 +8,7 @@ import torch
 
 
 # data preparation
-BATCH = 64
+BATCH = 32
 data, ids = load_imgs()
 train_data, valid_data = data[:8000], data[8000:9000]
 train_ids, valid_ids = ids[:8000], ids[8000:9000]
@@ -18,27 +18,27 @@ valid_loader = get_loader(valid_data, valid_labels[:, 0].reshape(-1, 1)/9, batch
 
 
 # model preparation
-# FC = [256, 64]
-# DROPOUT, CLS_BASE = 0.5, -1
-# device = "cuda" if torch.cuda.is_available() else "cpu"
-# model = VGGSpecModel(vgg11, 4096, 1, fcs=FC, dropout=DROPOUT,
-#                      classifier_base=CLS_BASE).half().to(device)
-FC = [128, 128, 64]
-DROPOUT = 0.5
+FC = [256, 128]
+DROPOUT, CLS_BASE = 0.5, -1
 device = "cuda" if torch.cuda.is_available() else "cpu"
-model = VGGishSpecModel(128, 1, fcs=FC, dropout=DROPOUT).half().to(device)
+model = VGGSpecModel(vgg16, 4096, 1, fcs=FC, dropout=DROPOUT,
+                     classifier_base=CLS_BASE).half().to(device)
+# FC = [128, 128, 64]
+# DROPOUT = 0.5
+# device = "cuda" if torch.cuda.is_available() else "cpu"
+# model = VGGishSpecModel(128, 1, fcs=FC, dropout=DROPOUT).half().to(device)
 
 
 # training params
-LR, MOMENTUM, DECAY = 0.001, 0.0, 0.0
+LR, MOMENTUM, DECAY = 0.001, 0.9, 0.01
 criterion = nn.L1Loss()
 optimizer = torch.optim.SGD(model.parameters(),
                             lr=LR, momentum=MOMENTUM, weight_decay=DECAY)
 
 
 # training
-EPOCHS = 10
+EPOCHS = 50
 best_model, losses = train_model(model, train_loader, criterion, optimizer, EPOCHS,
                                  device, valid_loader=valid_loader, half=True)
-# torch.save(model.state_dict(), f"spec_valence.pth")
-torch.save(model.state_dict(), f"spec_vggish_valence.pth")
+torch.save(model.state_dict(), f"spec_valence.pth")
+# torch.save(model.state_dict(), f"spec_vggish_valence.pth")
